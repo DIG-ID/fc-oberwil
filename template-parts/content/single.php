233966b81aa1e996a84130c6cdef052e466e82c2
pedro-digid <pedro@dig.id>
2022-06-30 16:44:07 +0100

    up

--- template-parts/content/single.php
+++ template-parts/content/single.php
@@ -14,8 +14,8 @@
 				<?php
 				the_post_navigation(
 					array(
-						'next_text' => '<div class="link-content"><span class="next-post">' . __( 'Next post', 'fc-oberwil' ) . '</span> ' . '<h4 class="post-title">%title</h4></div><span class="link-divider"></span><i class="fco-icon-chevron-right"></i>',
-						'prev_text' => '<i class="fco-icon-chevron-left"></i><span class="link-divider"></span><div class="link-content"><span class="previous-post">' . __( 'Previous post', 'fc-oberwil' ) . '</span> ' . '<h4 class="post-title">%title</h4></div>',
+						'next_text' => '<div class="link-content"><span class="next-post">' . __( 'Nächster Beitrag', 'fc-oberwil' ) . '</span> ' . '<h4 class="post-title">%title</h4></div><span class="link-divider"></span><i class="fco-icon-chevron-right"></i>',
+						'prev_text' => '<i class="fco-icon-chevron-left"></i><span class="link-divider"></span><div class="link-content"><span class="previous-post">' . __( 'Letzter Beitrag', 'fc-oberwil' ) . '</span> ' . '<h4 class="post-title">%title</h4></div>',
 					)
 				);
 				?>
