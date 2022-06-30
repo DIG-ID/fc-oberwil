d52419ded25f12f4192c8cbec8b2a3a194dece29
pedro-digid <pedro@dig.id>
2022-06-30 17:25:30 +0100

    up

--- header.php
+++ header.php
@@ -4,6 +4,15 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="theme-color" content="#D00224">
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-YV9YRLQT7P"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+
+			gtag('config', 'G-YV9YRLQT7P');
+		</script>
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
